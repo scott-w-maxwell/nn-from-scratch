8e5467031c9597fde8219213ec6e864e7771532a
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2021-04-28 07:53:30 -0700

    compacted scalable example

--- neuron_examples.py
+++ neuron_examples.py
@@ -32,21 +32,47 @@ bias1 = 2
 bias2 = 3
 bias3 = 0.5
 
-outputs = [
- # Neuron 1:
- inputs[0]*weights1[0] +
- inputs[1]*weights1[1] +
- inputs[2]*weights1[2] +
- inputs[3]*weights1[3] + bias1,
- # Neuron 2:
- inputs[0]*weights2[0] +
- inputs[1]*weights2[1] +
- inputs[2]*weights2[2] +
- inputs[3]*weights2[3] + bias2,
- # Neuron 3:
- inputs[0]*weights3[0] +
- inputs[1]*weights3[1] +
- inputs[2]*weights3[2] +
- inputs[3]*weights3[3] + bias3]
 
- print("Output for 3 layer neuron with 4 inputs", outputs)
+			 # Neuron 1:
+outputs = [	 inputs[0]*weights1[0] +
+			 inputs[1]*weights1[1] +
+			 inputs[2]*weights1[2] +
+			 inputs[3]*weights1[3] + bias1,
+			 # Neuron 2:
+			 inputs[0]*weights2[0] +
+			 inputs[1]*weights2[1] +
+			 inputs[2]*weights2[2] +
+			 inputs[3]*weights2[3] + bias2,
+			 # Neuron 3:
+			 inputs[0]*weights3[0] +
+			 inputs[1]*weights3[1] +
+			 inputs[2]*weights3[2] +
+			 inputs[3]*weights3[3] + bias3]
+
+print("Output for 3 layer neuron with 4 inputs", outputs)
+
+
+# compacted code (Same as above):
+
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1],
+ 		   [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
+# Output of current layer
+layer_outputs = []
+# For each neuron
+for neuron_weights, neuron_bias in zip(weights, biases):
+	# Zeroed output of given neuron
+	neuron_output = 0
+	# For each input and weight to the neuron
+	for n_input, weight in zip(inputs, neuron_weights):
+		# Multiply this input by associated weight
+		# and add to the neuron's output variable
+		neuron_output += n_input*weight
+	# Add bias
+	neuron_output += neuron_bias
+	# Put neuron's result to the layer's output list
+	layer_outputs.append(neuron_output)
+print("Output for same example but scalable code",layer_outputs)
+
